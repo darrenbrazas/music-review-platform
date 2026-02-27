bd0acb551026769ec27bc8553e82ab81bc18de8a
Darren Brazas <darrenbrazas2205@gmail.com>
2026-02-27 14:09:10 -0500

    Changed Back

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const APIBASE = "https://music-review-platform.onrender.com";
+
+
+const APIBASE = "http://localhost:5000";
 
 //load the artists so that they can be used
 
@@ -157,11 +159,13 @@ const displayAlbums = () => {
   
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (document.getElementById("albums")) {
-    loadArtists().then(displayAlbums).catch(console.error);
-  }
-});
+if (
+  window.location.pathname.endsWith("index.html") ||
+  window.location.pathname.endsWith("/") ||
+  window.location.pathname === "/music-review-platform"
+) {
+  loadArtists().then(displayAlbums).catch(console.error);
+}
 
     
 
